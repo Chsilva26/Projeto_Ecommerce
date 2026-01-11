ac560a88259f550b0c7d6a27c7551ca08d361b39
Carlos Peres <chsilvaperes@gmail.com>
2026-01-11 19:18:10 -0300

    1.0.2

---
+++ api/routes/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("express-jwt");
+const secret = require("../config").secret;
+
+function getTokenFromHeader(req){
+    if(!req.headers.authorization) return null;
+    const token = req.headers.authorization.split(" ");
+    if(token[0] !== "Ecommerce") return null;
+    return token[1];
+}
+
+"Ecommerce"
+
+const auth = {
+    required: jwt({
+        secret,
+        userProperty: 'payLoad',
+        getToken: getTokenFromHeader
+    }),
+    optional: jwt({
+        secret,
+        userProperty: 'payLoad',
+        credentialRequired: false,
+        getToken: getTokenFromHeader
+    })
+};
+
+module.exports = auth;
